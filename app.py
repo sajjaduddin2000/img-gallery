fab3168a127680c9381d855eb283efddd9c7e417
sajjaduddin2000 <sajjaduddin2000@outlook.com>
2025-03-19 02:54:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def upload_photos():
     return jsonify({"message": "Upload successful"})
 
 # Use Gunicorn for Production Deployment
-if __name__ == "__app__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
 
